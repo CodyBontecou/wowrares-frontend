5cfc83017e213469cd34d7b513fbaefa2c9cdc9c
Cody Bontecou <bontecouc@gmail.com>
2019-07-25 00:12:49 -0500

    Progress on landing page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <v-app dark>
+  <v-app dark>
     <v-toolbar>
       <v-menu offset-y>
         <v-toolbar-side-icon slot="activator"></v-toolbar-side-icon>
@@ -19,7 +18,8 @@
           </v-list>
         </v-container>
       </v-menu>
-      <!--      Toolbar section that will always show-->
+
+      <!--      Toolbar section that will always show  -->
       <v-toolbar-title class="text-uppercase grey--text">
         <span class="font-weight-light">Wow</span>
         <span>rares</span>
@@ -30,105 +30,117 @@
       <v-spacer></v-spacer>
     </v-toolbar>
 
-    <v-container grid-list-md text-xs-center>
-      <v-layout
-        row
-        wrap
-        class="pa-3 project">
-        <v-flex xs12 md9 v-if="zone">
-          <v-img
-            :src="zone.image"
-            alt=""
-          >
-            <div v-show="active">{{ mob.name }}<br>{{ mob.level }}<br>{{ mob.health }}</div>
-            <v-layout column fill-height class="lightbox white--text">
-              <v-flex shrink>
-                <div v-for="(mob, index) in mobs" :key="index">
-                  <v-img
-                    src="https://classicdb.ch/templates/wowhead/images/map-pin-yellow.png"
-                    alt=""
-                    height="11px"
-                    width="11px"
-                    :style="{
-                              top: mob.y_coordinate + '%',
-                              left: mob.x_coordinate + '%',
-                              position: 'absolute'
-                    }"
-                    @mouseover=mouseOver(mob)
-                    @mouseleave=mouseOff()
-                  >
-                  </v-img>
-                </div>
-              </v-flex>
-            </v-layout>
-          </v-img>
-        </v-flex>
-        <v-flex x12 md3>
-          <v-container
-            style="height: 550px"
-            class="scroll-y"
-          >
-            <v-expansion-panel>
-              <v-expansion-panel-content expand v-for="(mob, index) in mobs" :key="index">
-                <div slot="header">
-                  <v-card flat>
-                    <v-layout>
-                      <v-flex>
-                        <div class="caption grey--text">Name</div>
-                        <div>{{ mob.name }}</div>
-                      </v-flex>
-                      <v-flex>
-                        <div class="caption grey--text">Level</div>
-                        <div>{{ mob.level }}</div>
-                      </v-flex>
-                      <!--                      <v-flex xs8 md4 v-for="(item, index) in items" :key="index">-->
-                      <!--                        <v-img-->
-                      <!--                          :src=item.image-->
-                      <!--                          :lazy-src=item.lazy_image-->
-                      <!--                          max-height="40px"-->
-                      <!--                          max-width="40px"-->
-                      <!--                          @mouseover=hoverItem(item)-->
-                      <!--                        >-->
-                      <!--                        </v-img>-->
-                      <!--                      </v-flex>-->
-                    </v-layout>
-                  </v-card>
-                </div>
-                <v-card flat>
-                  <v-layout
-                    row
-                    wrap
-                    class="project">
-                    <v-flex xs6 align-self-center>
-                      <v-img
-                        :src="`${mob.image}`"
-                        alt=""></v-img>
-                    </v-flex>
-                    <v-flex align-self-center>
-                      <div class="caption grey--text">Damage</div>
-                      <div>{{ mob.damage }}</div>
-                    </v-flex>
-                    <v-flex align-self-center>
-                      <div class="caption grey--text">Health</div>
-                      <div>{{ mob.health }}</div>
-                    </v-flex>
-                  </v-layout>
-                </v-card>
-              </v-expansion-panel-content>
-            </v-expansion-panel>
-          </v-container>
-        </v-flex>
-        <Nuxt />
-      </v-layout>
-    </v-container>
+    <!--    <v-container grid-list-md text-xs-center>-->
+    <!--      <v-layout-->
+    <!--        row-->
+    <!--        wrap-->
+    <!--        class="pa-3 project">-->
+    <!--        <v-flex xs12 md9 v-if="zone">-->
+    <!--          <v-img-->
+    <!--            :src="zone.image"-->
+    <!--            alt=""-->
+    <!--          >-->
+    <!--            <div v-show="active">{{ mob.name }}<br>{{ mob.level }}<br>{{ mob.health }}</div>-->
+    <!--            <v-layout column fill-height class="lightbox white&#45;&#45;text">-->
+    <!--              <v-flex shrink>-->
+    <!--                <div v-for="(mob, index) in mobs" :key="index">-->
+    <!--                  <v-img-->
+    <!--                    src="https://classicdb.ch/templates/wowhead/images/map-pin-yellow.png"-->
+    <!--                    alt=""-->
+    <!--                    height="11px"-->
+    <!--                    width="11px"-->
+    <!--                    :style="{-->
+    <!--                              top: mob.y_coordinate + '%',-->
+    <!--                              left: mob.x_coordinate + '%',-->
+    <!--                              position: 'absolute'-->
+    <!--                    }"-->
+    <!--                    @mouseover=mouseOver(mob)-->
+    <!--                    @mouseleave=mouseOff()-->
+    <!--                  >-->
+    <!--                  </v-img>-->
+    <!--                </div>-->
+    <!--              </v-flex>-->
+    <!--            </v-layout>-->
+    <!--          </v-img>-->
+    <!--        </v-flex>-->
+    <!--        <v-flex x12 md3>-->
+    <!--          <v-container-->
+    <!--            style="height: 550px"-->
+    <!--            class="scroll-y"-->
+    <!--          >-->
+    <!--            <v-expansion-panel>-->
+    <!--              <v-expansion-panel-content expand v-for="(mob, index) in mobs" :key="index">-->
+    <!--                <div slot="header">-->
+    <!--                  <v-card flat>-->
+    <!--                    <v-layout>-->
+    <!--                      <v-flex>-->
+    <!--                        <div class="caption grey&#45;&#45;text">Name</div>-->
+    <!--                        <div>{{ mob.name }}</div>-->
+    <!--                      </v-flex>-->
+    <!--                      <v-flex>-->
+    <!--                        <div class="caption grey&#45;&#45;text">Level</div>-->
+    <!--                        <div>{{ mob.level }}</div>-->
+    <!--                      </v-flex>-->
+    <!--                      &lt;!&ndash;                      <v-flex xs8 md4 v-for="(item, index) in items" :key="index">&ndash;&gt;-->
+    <!--                      &lt;!&ndash;                        <v-img&ndash;&gt;-->
+    <!--                      &lt;!&ndash;                          :src=item.image&ndash;&gt;-->
+    <!--                      &lt;!&ndash;                          :lazy-src=item.lazy_image&ndash;&gt;-->
+    <!--                      &lt;!&ndash;                          max-height="40px"&ndash;&gt;-->
+    <!--                      &lt;!&ndash;                          max-width="40px"&ndash;&gt;-->
+    <!--                      &lt;!&ndash;                          @mouseover=hoverItem(item)&ndash;&gt;-->
+    <!--                      &lt;!&ndash;                        >&ndash;&gt;-->
+    <!--                      &lt;!&ndash;                        </v-img>&ndash;&gt;-->
+    <!--                      &lt;!&ndash;                      </v-flex>&ndash;&gt;-->
+    <!--                    </v-layout>-->
+    <!--                  </v-card>-->
+    <!--                </div>-->
+    <!--                <v-card flat>-->
+    <!--                  <v-layout-->
+    <!--                    row-->
+    <!--                    wrap-->
+    <!--                    class="project">-->
+    <!--                    <v-flex xs6 align-self-center>-->
+    <!--                      <v-img-->
+    <!--                        :src="`${mob.image}`"-->
+    <!--                        alt=""></v-img>-->
+    <!--                    </v-flex>-->
+    <!--                    <v-flex align-self-center>-->
+    <!--                      <div class="caption grey&#45;&#45;text">Damage</div>-->
+    <!--                      <div>{{ mob.damage }}</div>-->
+    <!--                    </v-flex>-->
+    <!--                    <v-flex align-self-center>-->
+    <!--                      <div class="caption grey&#45;&#45;text">Health</div>-->
+    <!--                      <div>{{ mob.health }}</div>-->
+    <!--                    </v-flex>-->
+    <!--                  </v-layout>-->
+    <!--                </v-card>-->
+    <!--              </v-expansion-panel-content>-->
+    <!--            </v-expansion-panel>-->
+    <!--          </v-container>-->
+    <!--        </v-flex>-->
+    <!--      </v-layout>-->
+    <!--    </v-container>-->
 
-    <v-footer ma-3>
-      <a href="https://www.patreon.com/bePatron?u=20293765" data-patreon-widget-type="become-patron-button">Become a
-        Patron!</a>
-      <script async src="https://c6.patreon.com/becomePatronButton.bundle.js"></script>
+    <Nuxt/>
+
+    <v-footer
+      dark
+      padless
+    >
+      <v-card
+        flat
+        tile
+        class="text-center"
+      >
+        <v-card-text class="white--text">
+          <a href="https://www.patreon.com/bePatron?u=20293765" data-patreon-widget-type="become-patron-button">Become a
+            Patron!</a>
+          <script async src="https://c6.patreon.com/becomePatronButton.bundle.js"></script>
+        </v-card-text>
+      </v-card>
     </v-footer>
+
   </v-app>
-  </div>
 </template>
 
 <script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,49 @@
 <template>
-  <div>
-    <h1 class="white--text">Home Page</h1>
-  </div>
+  <v-container grid-list-md>
+    <v-layout align-top justify-center row wrap class="pa-5">
+      <v-flex xs12>
+        <p class="text-uppercase grey--text display-1 font-weight-medium">
+          <span class="font-weight-light">Wow</span>
+          <span>rares</span>
+        </p>
+        <v-form ref="form" v-model="valid" :lazy-validation="lazy">
+          <v-text-field solo v-model="zone" label="Zone name..." append-icon="search" @click:append required></v-text-field>
+        </v-form>
+      </v-flex>
+    </v-layout>
+    <v-layout wrap>
+      <v-flex d-flex xs12>
+        <p class="grey--text font-weight-medium">
+          <span class="">Introducing Wowrares.com</span>
+          <span class="font-weight-light">A tool to optimize your WoW Classic experience.</span>
+        </p>
+      </v-flex>
+      <v-flex d-flex xs12 md4>
+        <v-card color="dark-grey">
+          <v-card-title primary class="title">Navigate</v-card-title>
+          <v-img src="nav-cropped.png"></v-img>
+        </v-card>
+      </v-flex>
+      <v-flex d-flex xs12 md4>
+        <v-card>
+          <v-card-title primary class="title">Locate</v-card-title>
+          <v-img src="timber-only-map.png"></v-img>
+        </v-card>
+      </v-flex>
+      <v-flex d-flex xs12 md4>
+        <v-card>
+          <v-card-title class="title">Loot</v-card-title>
+          <v-img src="timber-loot-cropped.png"></v-img>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 <script>
   export default {
+    data: () => ({
+      lorem: `Lorem ipsum dolor sit amet, mel at clita quando. Te sit oratio vituperatoribus, nam ad ipsum posidonium mediocritatem, explicari dissentiunt cu mea. Repudiare disputationi vim in, mollis iriure nec cu, alienum argumentum ius ad. Pri eu justo aeque torquatos.`,
+    }),
     async fetch({store}) {
       await store.dispatch('fetchAllZones');
     },
